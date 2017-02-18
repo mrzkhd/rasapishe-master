45ee8466f00ac40bc7e5627a8bfd08087fb94cae
Mohammad Reza Khodabandeh <Mohammadreza@Mohammads-MacBook-Air.local>
2017-02-18 10:20:04 +0330

    add favorite list

--- app/src/main/java/com/rasapishe/customer/screen/HomeActivity.java
+++ app/src/main/java/com/rasapishe/customer/screen/HomeActivity.java
@@ -16,78 +16,86 @@ import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class HomeActivity extends AppCompatActivity {
 
-    SearchView searchView;
+	SearchView searchView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.setTitle(getTitle());
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		toolbar.setTitle(getTitle());
 
-        // Show the Up button in the action bar.
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+		// Show the Up button in the action bar.
+		ActionBar actionBar = getSupportActionBar();
+		if (actionBar != null) {
+			actionBar.setDisplayHomeAsUpEnabled(true);
+		}
 
-        searchView = (SearchView)findViewById(R.id.searchview);
-        if(searchView != null)
-            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-                @Override
-                public boolean onQueryTextSubmit(String query) {
-                    Intent intent = new Intent(HomeActivity.this, BusinessListActivity.class);
-                    intent.putExtra(BusinessListActivity.QUERY_KEY, query);
-                    startActivity(intent);
-                    return true;
-                }
+		searchView = (SearchView) findViewById(R.id.searchview);
+		if (searchView != null)
+			searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+				@Override
+				public boolean onQueryTextSubmit(String query) {
+					Intent intent = new Intent(HomeActivity.this, BusinessListActivity.class);
+					intent.putExtra(BusinessListActivity.QUERY_KEY, query);
+					startActivity(intent);
+					return true;
+				}
 
-                @Override
-                public boolean onQueryTextChange(String newText) {
-                    return false;
-                }
-            });
+				@Override
+				public boolean onQueryTextChange(String newText) {
+					return false;
+				}
+			});
 
-        findViewById(R.id.allBusinessButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, BusinessListActivity.class);
-                intent.putExtra(BusinessListActivity.TITLE_KEY, R.string.btn_category_business);
-                startActivity(intent);
-            }
-        });
+		findViewById(R.id.allBusinessButton).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(HomeActivity.this, BusinessListActivity.class);
+				intent.putExtra(BusinessListActivity.TITLE_KEY, R.string.btn_category_business);
+				startActivity(intent);
+			}
+		});
+		findViewById(R.id.button_favorite).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(HomeActivity.this, FavoriteListActivity.class);
+				intent.putExtra(BusinessListActivity.TITLE_KEY, R.string.btn_favorite_business);
+				startActivity(intent);
+			}
+		});
 
 
-        findViewById(R.id.factors_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, FactorListActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
+		findViewById(R.id.factors_button).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(HomeActivity.this, FactorListActivity.class);
+				startActivity(intent);
+			}
+		});
+	}
 
-    @Override
-    protected void attachBaseContext(Context newBase) {
-        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
-    }
+	@Override
+	protected void attachBaseContext(Context newBase) {
+		super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == android.R.id.home) {
-            // This ID represents the Home or Up button. In the case of this
-            // activity, the Up button is shown. Use NavUtils to allow users
-            // to navigate up one level in the application structure. For
-            // more details, see the Navigation pattern on Android Design:
-            //
-            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-            //
-           finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		if (id == android.R.id.home) {
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			finish();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
